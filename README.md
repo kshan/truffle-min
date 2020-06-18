884cbfb451d866cfa3013c433e821d6f10356a59
xilibi2003@163.com <xilibi2003@163.com>
2020-06-18 15:01:21 +0800

    Add truffle-min.sh

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # truffle-min
-Minimizing truffle json artifact
+
+A Script for minimizing truffle json artifact.
+
+一个精简 Truffle 构建文件（artifact）的脚本。
+
+
+
+## Usage
+
+Copy truffle-min.sh to your Truffle project dir, Then run:
+
+```
+./truffle-min.sh build/contracts
+```
+
+build/contracts is the artifact dir
+
+
+## 用法
+拷贝  truffle-min.sh 到 Truffle 项目根目录，再运行：
+
+```
+./truffle-min.sh build/contracts
+```
+
+
---
+++ truffle-min.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Notes:
+#  - Shchema: https://github.com/trufflesuite/truffle/tree/develop/packages/truffle-contract-schema
+#  - bytecode vs deployedBytecode: https://ethereum.stackexchange.com/questions/32234/difference-between-bytecode-and-runtime-bytecode
+for i in "$1"/*.json;do
+  [ -z "${i%%*.min.json}" ] && continue # already minified
+  m=${i%%.json}.min.json
+  echo "Minimizing truffle json artifact: $i"
+  echo "Original size: $(wc -c "$i")"
+  jq 'del(.ast,.legacyAST,.source,.deployedSourceMap,.userdoc,.sourcePath,.bytecode,.deployedBytecode,.sourceMap,.metadata,.compiler,.devdoc,.schemaVersion)' $i > $m
+  echo "Minimized size: $(wc -c "$m")"
+  mv "$m" "$1"/min/ 
+done
